0a3b50114679ca1f338123877265006adc7d5fa3
Alexander <7319abowers@gmail.com>
2021-11-13 12:40:09 -0800

    added exit functionality

--- ATM app/ATM app.cpp
+++ ATM app/ATM app.cpp
@@ -9,6 +9,7 @@ void showMenu() {
     cout << "1. Check balance" << endl;
     cout << "2. Deposit" << endl;
     cout << "3. Withdraw" << endl;
+    cout << "4. Exit" << endl;
     cout << "************************" << endl;
 }
 
@@ -18,27 +19,27 @@ int main()
     int option;
     double balance = 500;
 
+    do {
+        showMenu();
+        cout << "Option: ";
+        cin >> option;
 
-    showMenu();
-    cout << "Option: ";
-    cin >> option;
-
-    switch (option) {
-    case 1:cout << "Balance is: $" << balance << "" << endl; break;
-    case 2:cout << "Deposit amount:";
-        double depositAmount;
-        cin >> depositAmount;
-        balance += depositAmount; break;
-    case 3:cout << "Withdraw amount:";
-        double withdrawAmount;
-        cin >> withdrawAmount;
-        if (withdrawAmount <= balance)
-            balance -= withdrawAmount;
-        else
-            cout << "Not enough money in account" << endl;
-        break;
-
-    }
+        switch (option) {
+        case 1:cout << "Balance is: $" << balance << "" << endl; break;
+        case 2:cout << "Deposit amount:";
+            double depositAmount;
+            cin >> depositAmount;
+            balance += depositAmount; break;
+        case 3:cout << "Withdraw amount:";
+            double withdrawAmount;
+            cin >> withdrawAmount;
+            if (withdrawAmount <= balance)
+                balance -= withdrawAmount;
+            else
+                cout << "Not enough money in account" << endl;
+            break;
+        }
+    } while (option != 4);
 
     system("pause>0");
     
