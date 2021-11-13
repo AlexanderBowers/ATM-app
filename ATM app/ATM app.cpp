0211734762cc890d39ebf31561183e2349ef1e07
Alexander <7319abowers@gmail.com>
2021-11-13 12:47:58 -0800

    added clear screen and formatting changes

--- ATM app/ATM app.cpp
+++ ATM app/ATM app.cpp
@@ -23,20 +23,31 @@ int main()
         showMenu();
         cout << "Option: ";
         cin >> option;
+        system("cls");
 
         switch (option) {
-        case 1:cout << "Balance is: $" << balance << "" << endl; break;
-        case 2:cout << "Deposit amount:";
+        case 1:
+            cout << "Balance is: $" << balance << "" << endl;
+            break;
+        case 2:
+            cout << "Balance is: $" << balance << "" << endl;
+            cout << "Deposit amount:";
             double depositAmount;
             cin >> depositAmount;
-            balance += depositAmount; break;
-        case 3:cout << "Withdraw amount:";
+            balance += depositAmount;
+            break;
+        case 3:
+            cout << "Balance is: $" << balance << "" << endl;
+            cout << "Withdraw amount:";
             double withdrawAmount;
             cin >> withdrawAmount;
             if (withdrawAmount <= balance)
                 balance -= withdrawAmount;
             else
                 cout << "Not enough money in account" << endl;
+            break;
+        default:
+            cout << "Please select a valid option" << endl;
             break;
         }
     } while (option != 4);
