67c7acf460c8191e4a7019abe2a261f761e7f02a
Alexander <7319abowers@gmail.com>
2021-11-13 12:37:07 -0800

    added switch case after menu is called

--- ATM app/ATM app.cpp
+++ ATM app/ATM app.cpp
@@ -15,7 +15,31 @@ void showMenu() {
 int main()
 {
     //check balance, deposit, withdraw, show menu
+    int option;
+    double balance = 500;
+
+
     showMenu();
+    cout << "Option: ";
+    cin >> option;
+
+    switch (option) {
+    case 1:cout << "Balance is: $" << balance << "" << endl; break;
+    case 2:cout << "Deposit amount:";
+        double depositAmount;
+        cin >> depositAmount;
+        balance += depositAmount; break;
+    case 3:cout << "Withdraw amount:";
+        double withdrawAmount;
+        cin >> withdrawAmount;
+        if (withdrawAmount <= balance)
+            balance -= withdrawAmount;
+        else
+            cout << "Not enough money in account" << endl;
+        break;
+
+    }
+
     system("pause>0");
     
 }
