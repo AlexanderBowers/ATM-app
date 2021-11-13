78bf0df4e250a1499b3a098ba7952cb258989cb7
Alexander <7319abowers@gmail.com>
2021-11-13 12:26:55 -0800

    created showMenu function and called it

--- ATM app/ATM app.cpp
+++ ATM app/ATM app.cpp
@@ -4,9 +4,19 @@
 #include <iostream>
 using namespace std;
 
+void showMenu() {
+    cout << "**********MENU**********" << endl;
+    cout << "1. Check balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "************************" << endl;
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    //check balance, deposit, withdraw, show menu
+    showMenu();
+    system("pause>0");
     
 }
 
